c11da83a3e7f50c8ebe7d0f5cd6ab3861134f9ef
uc595154 <uc595154@dal.ca>
2026-05-19 15:41:11 -0300

    Add Lab1, Lab2, Lab3 and Netmiko lab

---
+++ Lab3/task3.py
@@ -0,0 +1,13 @@
+
+
+from jinja2 import Environment, FileSystemLoader
+
+ENV = Environment(loader=FileSystemLoader('.'))
+template = ENV.get_template("template-task3.j2")
+
+interface_data = {
+    "description": "Server Port",
+    "vlan": 10
+}
+
+print(template.render(interface=interface_data))
